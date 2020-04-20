026e536a57ae54c9a94a1cd3c499830a75edb824
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-04-20 10:58:59 -0300

    Restauração da tela de listas

--- lib/telas/tela_listas.dart
+++ lib/telas/tela_listas.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:listacompras/componentes/item_categoria.dart';
 
 class TelaListas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return Column(
-          children: <Widget>[
-            ItemCategoria(nome: 'Frutas'),
-          ],
+        return Center(
+          child: Text('Inicio'),
         );
       },
     );
